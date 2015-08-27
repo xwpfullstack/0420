842aa7fd622797f9b8488de64bec2b2d2bad96ae
xingwenpeng <xwp_fullstack@163.com>
2015-08-27 16:25:40 +0800

    create b.c

---
+++ b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello\n");
+    printf("hello\n");
+    printf("hello\n");
+    printf("hello\n");
+    printf("hello\n");
+    printf("hello\n");
+    printf("hello\n");
+    return 0;
+}
